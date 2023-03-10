16e3ecb2403905a09e9f5269dd510da2a59a5b64
ENUGU VISHWANTH REDDY <93393384+Vishwanth123@users.noreply.github.com>
2023-03-11 02:35:50 +0530

    created video player page

---
+++ index.js
@@ -0,0 +1,8 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     var video1 = document.getElementById("video1");
+//     var myVideo = document.getElementById("myVideo");
+
+//     video1.addEventListener("click", function() {
+//         myVideo.src = "videos/video1.mp4";
+//     });
+// });
