c51b32ad32dc8f00c29f5b2451f915d0044edef9
ENUGU VISHWANTH REDDY <93393384+Vishwanth123@users.noreply.github.com>
2023-03-11 17:27:41 +0530

    icons in sidenav hyperlinks

--- index.js
+++
@@ -1,8 +0,0 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     var video1 = document.getElementById("video1");
-//     var myVideo = document.getElementById("myVideo");
-
-//     video1.addEventListener("click", function() {
-//         myVideo.src = "videos/video1.mp4";
-//     });
-// });
